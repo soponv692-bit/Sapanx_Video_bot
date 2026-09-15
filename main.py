195e9594ab71f2a262f5f4b90f99b4f92f213edc
soponv692-bit <soponv692@gmail.com>
2026-09-15 13:06:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os; os.system("python3 -m http.server $PORT &")
 import os, asyncio, nest_asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy import VideoFileClip, concatenate_videoclips
 
 nest_asyncio.apply()
 
